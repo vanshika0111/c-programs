e4bf2ad527712d75413d07d43ae955b6fc843ab2
Vanshika <vanshikag0111@gmail.com>
2021-06-05 19:19:21 +0530

    code that gives 7 seven digit as output

---
+++ RECURSION & FUNCTIONS/six digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include<stdlib.h>
+
+#include<time.h>
+
+int main()
+
+{
+    
+    int number;
+
+    srand(time(0));
+
+    printf("\n The number is %d",number);
+
+    return 0;
+
+}
