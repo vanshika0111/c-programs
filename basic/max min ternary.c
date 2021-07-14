c34f675dc6dc8f1eda7d0fb317283befa1b06749
Vanshika <vanshikag0111@gmail.com>
2021-07-14 22:44:58 +0530

    corrected the indentation

--- basic/max min ternary.c
+++ basic/max min ternary.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+
 int main()
+
 {
-    int x,y,max,min;
-    printf("enter two numbers:");
-    scanf("%d %d",&x,&y);
-    max=(x>y)?x:y;
-    min=(x<y)?x:y;
-    printf("max= %d\n",max);
-    printf("min= %d\n",min);
-    return 0;
+    
+	int x,y,max,min;
+    
+	printf("enter two numbers:");
+    
+	scanf("%d %d",&x,&y);
+    
+	max=(x>y)?x:y;
+    
+	min=(x<y)?x:y;
+    
+	printf("max= %d\n",max);
+    
+	printf("min= %d\n",min);
+    
+	return 0;
+
 }
